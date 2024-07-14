eefe6bd7dbb222309310c192b705a9131c740b53
shady070 <malikx029squaddie@gmail.com>
2024-07-14 20:36:20 +0500

    Comming Soon

---
+++ app/startquiz/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='md:text-[40px] text-[24px] flex items-center justify-center text-center'>
+        Comming Soon
+    </div>
+  )
+}
+
+export default page
