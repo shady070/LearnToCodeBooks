ea6c7215d6a862fb3408faac9fe61242e6b63d0d
shady070 <malikx029squaddie@gmail.com>
2024-07-14 20:44:36 +0500

    Comming Soon-1

--- app/startquiz/page.tsx
+++ app/startquiz/page.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div className='py-[300px] md:text-[40px] text-[24px] flex items-center justify-center text-center'>
+    <div className='text-white py-[300px] md:text-[40px] text-[24px] flex items-center justify-center text-center'>
         Comming Soon
     </div>
   )
